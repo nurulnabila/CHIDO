30430caa6b507625d609d18487e2e47a533e5d8d
Nurul Nabila <200695nurulnabila@users.noreply.github.com>
2017-12-29 01:50:59 +0800

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/fragrancecart/BrowniesFragment.java
@@ -0,0 +1,34 @@
+package com.delaroystudios.fragrancecart;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BrowniesFragment extends Fragment {
+
+
+    public BrowniesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_brownies, container, false);
+
+
+    }
+
+}
